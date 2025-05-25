cb50b498c0c6e89dd455b9a78e6e55165939487e
suraj-bv <surajbv5566@gamil.com>
2025-05-25 18:02:16 +0530

    Fix: remove localhost from API call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/message')
+    fetch('/api/message')
       .then(res => res.json())
       .then(data => setMessage(data.message))
       .catch(err => console.error(err));
